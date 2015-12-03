801c62a353c4c1ebaacc33fe556bb351c1cfe8c5
Roj <joa.dev@live.com>
2015-12-03 07:43:39 -0300

    tda funciona. agregado pruebas

--- archivos.jl
+++ archivos.jl
@@ -2,10 +2,10 @@ include("graph.jl")
 
 type Ciudad{}
 	nombre :: AbstractString
-	longitud :: Int64
-	latitud :: Int64
-	provincia :: Int32
-	habitantes :: Int32
+	longitud :: Float64
+	latitud :: Float64
+	provincia :: Int64
+	habitantes :: Int64
 end
 
 typealias Nodo_Ciudad Nodo{Ciudad}
@@ -13,49 +13,49 @@ typealias Nodo_Ciudad Nodo{Ciudad}
 type Ruta{}
 	id_ciudad_1 :: Int32
 	id_ciudad_2 :: Int32
-	puntaje :: Int32
-	distancia :: Int32 
+	puntaje :: Float64
+	distancia :: Float64 
 end
 
 
 #Recibe un mapa (grafo) y agrega las ciudades
 function procesar_ciudades(mapa::Grafo, nombre_archivo::AbstractString)
+	#Crea una tupla, primer elemento una matriz con datos, segundo un array del header
 	arch_ciudades = readdlm(nombre_archivo,'\t',header=true) 
-		#Crea una tupla, primer elemento una matriz con datos, segundo un array del header
+	#Dejamos en "ciudades" la matriz
 	ciudades = arch_ciudades[1]
 	cant_filas = size(ciudades)[1] 
-	mapa = Grafo(0,Dict()) #Creo grafo vacio
 	for i in 1:cant_filas
-		id = ciudades[i,:][1]
-		nombre = ciudades[i,:][2]
-		longitud = ciudades[i,:][3]
-		latitutd = ciudades[i,:][4]
-		provincia = ciudades[i,:][5]
-		habitantes = ciudades[i,:][6]
+		id = ciudades[i,:][1] #Int64
+		nombre = ciudades[i,:][2] #SubString
+		longitud = ciudades[i,:][3] #Float64
+		latitud = ciudades[i,:][4] #Float64
+		provincia = ciudades[i,:][5] #Int64
+		habitantes = ciudades[i,:][6] #Int64
 		ciudad = Ciudad(nombre,longitud,latitud,provincia,habitantes)
 		nodo_ciudad = Nodo_Ciudad(ciudad,Dict())
-		agregar_nodo(mapa,id,nodo_ciudad)
+		agregar_nodo(mapa,string(id),nodo_ciudad)
     end
  end
 
 
-#Recibe un mapa (grafo) y agrega las rutas, devuelve un diccionario con todas las rutas
+#Recibe un mapa (grafo) y agrega las rutas(aristas)
 function procesar_rutas(mapa::Grafo, nombre_archivo::AbstractString)
 	arch_rutas = readdlm(nombre_archivo,'\t',header=true) 
 	rutas = arch_rutas[1]
 	cant_filas = size(rutas)[1] 
-	rutas = Dict{AbstractString,Ruta}
 	for i in 1:cant_filas
-		id = rutas[i,:][1]
-		id_ciudad_1 = rutas[i,:][2]
-		id_ciudad_2 = rutas[i,:][3]
+		id = convert(Int64,rutas[i,:][1])
+		id_ciudad_1 = convert(Int64,rutas[i,:][2]) #viene como Float64
+		id_ciudad_2 = convert(Int64,rutas[i,:][3]) #idem
 		puntaje = rutas[i,:][4]
-		distancia = distancia[i,:][5]
+		distancia = rutas[i,:][5]
 		ruta = Ruta(id_ciudad_1,id_ciudad_2,puntaje,distancia)
-		agregar_arista(mapa,id)
-		rutas[id] = ruta
+		#void agregar_arista(Grafo grafo, String origen, String destino, String id, Void* Objeto)
+		agregar_arista(mapa,string(id_ciudad_1),string(id_ciudad_2),string(id),ruta)
+
+		
     end
-    return rutas
 end
 
  
--- graph.jl
+++ graph.jl
@@ -1,42 +1,64 @@
 type Nodo{Propiedades}
 	propiedades :: Propiedades
-	adyacentes :: Dict{AbstractString,Int} #Clave: String Valor:Int
+	#El diccionario almacena (clave,valor) == (destino, indice)
+	#donde "indice" es la referencia a la arista, que esta en el grafo
+	adyacentes :: Dict{AbstractString,AbstractString} #Clave: String Valor:String
 end
 
-type Grafo
+type Grafo{PropiedadesNodo,PropiedadesArista}
 	tamanio::Int
-	nodos::Dict{AbstractString,Nodo{Any}}
+	nodos::Dict{AbstractString,Nodo{PropiedadesNodo}}
+	aristas::Dict{AbstractString, PropiedadesArista}
+	#Constructor
+	#No tiene parametros; siempre se inicia en 0
+	#y las claves de los diccionarios deben ser AbstractString
+	function Grafo()
+		new(0,Dict{AbstractString,PropiedadesNodo}(),Dict{AbstractString,PropiedadesArista}())
+	end
 end
 
 
 function agregar_nodo(grafo::Grafo,clave::AbstractString,nodo::Nodo)
-	if !(clave in keys(grafo.nodos))
-		grafo.nodos[clave]=Nodo
+	if !haskey(grafo.nodos,clave)
+		grafo.nodos[clave]=nodo
 		grafo.tamanio += 1
 	end
 end
-
-function agregar_arista(grafo::Grafo,clave_origen::AbstractString,clave_destino::AbstractString,peso::Int)
-	if (clave_origen in keys(grafo.nodos)) & (clave_destino in keys(grafo.nodos))
-		grafo.nodos[clave_origen].adyacentes[clave_destino] = peso
+#void agregar_arista(Grafo grafo, String origen, String destino, String id, Void* Objeto)
+function agregar_arista(grafo::Grafo,clave_origen::AbstractString,clave_destino::AbstractString, id_arista::AbstractString, prop_arista::Any)
+	if haskey(grafo.nodos,clave_origen) && haskey(grafo.nodos,clave_destino)
+		grafo.aristas[id_arista] = prop_arista
+		grafo.nodos[clave_origen].adyacentes[clave_destino] = id_arista
+		grafo.nodos[clave_destino].adyacentes[clave_origen] = id_arista
 	end
 end
 
 function ver_adyacentes(grafo::Grafo, clave::AbstractString)
-	if (clave in keys(grafo.nodos))
-		return grafo.nodos[clave].adyacentes
+	if haskey(grafo.nodos,clave)
+		return keys(grafo.nodos[clave].adyacentes)
 	end
 end
 
 function pertenece(grafo::Grafo, clave::AbstractString)
-	if (clave in keys(grafo.nodos))
-		return True
-	end
-	return False
+	return haskey(grafo.nodos,clave)
 end
 
+function arista_existe(grafo::Grafo, origen::AbstractString, destino::AbstractString) 
+	return pertenece(grafo,origen) && pertenece(grafo,destino) && haskey(grafo.nodos[origen].adyacentes,destino)
+end
+
+function arista_existe_id(grafo::Grafo, id_arista::AbstractString)
+	return haskey(grafo.aristas,id_arista)
+end
+
+function arista_obtener(grafo::Grafo, origen::AbstractString, destino::AbstractString)
+	arista_existe(grafo,origen,destino) && return grafo.aristas[grafo.nodos[origen].adyacentes[destino]]
+	return false # en caso de que no exista
+end
+
+#esto no va a funcionar(se puede corregir despues, no es necesario por ahora..)
 function borrar_nodo(grafo::Grafo,clave::AbstractString)
-	if (clave in keys(grafo.nodos))
+	if haskey(grafo.nodos,clave)
 		for nodo in values(grafo.nodos)
 			delete!(nodo.adyacentes,clave)
 		end
@@ -45,7 +67,7 @@ function borrar_nodo(grafo::Grafo,clave::AbstractString)
 end
 
 function borrar_arista(grafo::Grafo,clave_origen::AbstractString,clave_destino::AbstractString)
-	if (clave_origen in keys(grafo.nodos)) & (clave_destino in keys(grafo.nodos))
+	if haskey(grafo.nodos,clave_origen) && haskey(grafo.nodos,clave_destino)
 		delete!(grafo.nodos[clave_origen].adyacentes,clave_destino)
 	end
 end
