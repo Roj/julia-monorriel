27efbf55944d1ec5c5cd5bfb22d641119c5b4aba
tutecano1995 <tutecano1995@gmail.com>
2015-12-07 21:09:56 -0300

    funciona!

--- export_kml.jl
+++ export_kml.jl
@@ -1,24 +1,24 @@
-function agregar_lugares_de_interes(ciudades::Dict{Int64,Nodo_Ciudad},arch::IOStream)
-	write(arch,("<Placemark>"))
+function agregar_lugares_de_interes(ciudades::Dict{AbstractString,Nodo_Ciudad},arch::IOStream)
 	for ciudad in values(ciudades)
-		write(arch,join(("\t<name>",ciudad.nombre,"</name>")))
-		write(arch,"\t<Point>")
-		write(arch,join(("\t\t<coordinates>",ciudad.longitud,", ",ciudad.latitud,"</coordinates>")))
-		write(arch,"\t</Point>")
+		write(arch,("\t\t<Placemark>\n"))
+		write(arch,join(("\t\t\t<name>",ciudad.propiedades.nombre,"</name>\n")))
+		write(arch,"\t\t\t<Point>\n")
+		write(arch,join(("\t\t\t\t<coordinates>",ciudad.propiedades.longitud,", ",ciudad.propiedades.latitud,"</coordinates>\n")))
+		write(arch,"\t\t\t</Point>\n")
+		write(arch,("\t\t</Placemark>\n\n"))
 	end
-	write(arch,("</Placemark>"))
 end
 
-function agregar_rutas(ciudades::Dict{Int64,Nodo_Ciudad},rutas::Dict{Int64,Ruta},arch::IOStream) #¿HAY UNA FORMA MAS LINA DE DEFINIR TODO ESTO?, PODRIAMOS HACER UN TIPO MAPA
-	write(arch,("<Placemark>"))
+function agregar_rutas(ciudades::Dict{AbstractString,Nodo_Ciudad},rutas::Dict{AbstractString,Ruta},arch::IOStream) #¿HAY UNA FORMA MAS LINA DE DEFINIR TODO ESTO?, PODRIAMOS HACER UN TIPO MAPA
 	for ruta in values(rutas)
-		write(arch,"\t<LineString>")
-		ciudad_1 = ciudades[ruta.id_ciudad_1]
-		ciudad_2 = ciudades [ruta.id_ciudad_2]
-		write(arch,join(("\t\t<coordinates>",ciudad_1.longitud,", ",ciudad_1.latitud," ",ciudad_2.longitud,", ",ciudad_2.latitud, "</coordinates>")))
-		write(arch,"\t</LineString>")
+		write(arch,("\t\t<Placemark>\n"))
+		write(arch,"\t\t\t<LineString>\n")
+		ciudad_1 = ciudades[string(ruta.id_ciudad_1)]
+		ciudad_2 = ciudades[string(ruta.id_ciudad_2)]
+		write(arch,join(("\t\t\t\t<coordinates>",ciudad_1.propiedades.longitud,", ",ciudad_1.propiedades.latitud," ",ciudad_2.propiedades.longitud,", ",ciudad_2.propiedades.latitud, "</coordinates>\n")))
+		write(arch,"\t\t\t</LineString>\n")
+		write(arch,("\t\t</Placemark>\n\n"))
 	end
-	write(arch,("</Placemark>"))
 end	
 
 function exportar_grafo_kml(mapa::Grafo,nombre_archivo::AbstractString)
@@ -26,10 +26,10 @@ function exportar_grafo_kml(mapa::Grafo,nombre_archivo::AbstractString)
 	write(arch,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
 	write(arch,"<kml xmlns=\"http://earth.google.com/kml/2.1\">\n")
 	write(arch,"\t<Document>\n")
-	write(arch,join(("<name>",nombre_archivo,"</name>\n")))
-	
+	write(arch,join(("\t\t<name>",nombre_archivo,"</name>\n\n")))
+
 	agregar_lugares_de_interes(mapa.nodos,arch)
-	agregar_rutas(mapa.aristas,arch)
+	agregar_rutas(mapa.nodos,mapa.aristas,arch)
 
 	write(arch,"\t</Document>\n")
 	write(arch,"</kml>\n")
